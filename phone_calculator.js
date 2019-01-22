43defa567a9f58c5d41c3c3281addd105d8bd8b0
Seul Lee <27seul.lee@gmail.com>
2019-01-21 21:49:57 -0500

    Fixed spacing

--- phone_calculator.js
+++ phone_calculator.js
@@ -1,7 +1,7 @@
 /**
 Exercise from "You Don't Know JS"
 
-Write a program to calculate the total price of your phone purchase.You will keep purchasing phones(hint: loop!) until you run out of money in your bank account. You'll also buy accessories for each phone as long as your purchase amount is below your mental spending threshold.
+Write a program to calculate the total price of your phone purchase. You will keep purchasing phones(hint: loop!) until you run out of money in your bank account. You'll also buy accessories for each phone as long as your purchase amount is below your mental spending threshold.
 
 * After you 've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted.
 
@@ -12,8 +12,7 @@ Write a program to calculate the total price of your phone purchase.You will kee
 * You should define functions for calculating the tax and
 for formatting the price with a "$" and rounding to two decimal places.
 
-* Bonus Challenge: Try to incorporate input into this program, perhaps with the prompt(..) covered in "Input"
-earlier.You may prompt the user for their bank account balance, for example. Have fun and be creative!
+* Bonus Challenge: Try to incorporate input into this program, perhaps with the prompt(..) covered in "Input" earlier. You may prompt the user for their bank account balance, for example. Have fun and be creative!
 **/
 
 // Set phone price
@@ -29,21 +28,25 @@ const tax_rate = 0.13;
 const spending_threshold = 800;
 
 // Set bank account balance
-var account_balance;
+var account_balance = 2000;
 
 // Set total purchase cost
 var total_cost;
 
 // Function for calculating the tax
-const tax_calc = function(phone_price, tax_rate) {
-  var tax_total;
-  tax_total = phone_price * tax_rate;
-  return tax_total;
+function tax_calc(phone_price, tax_rate) {
+  return phone_price * tax_rate;
 };
 
-//function for formatting the price with a "$" and rounding to two decimal places
-const formatting = function(total_cost) {
-  console.log('$' + total_cost.toFixed(2));
+// Function for formatting the price with a "$" and rounding to two decimal places
+function formatting(total_cost) {
+  return '$' + total_cost.toFixed(2);
 };
 
-= phone_price + tax_total(phone_price, tax_rate);
+// Calculate total cost of phone purchase
+total_cost = phone_price + tax_calc(phone_price, tax_rate);
+
+// Purchase phone until you run out of balance
+while (total_cost < account_balance) {
+  total_cost = total_cost + total_cost;
+}
